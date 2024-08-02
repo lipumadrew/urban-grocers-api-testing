c0c86bc5698c96ad6d2993ce649739494be88d06
Andrew Lipuma <andrewlipuma@gmail.com>
2024-08-01 19:40:59 -0700

    add PUT test to check status

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-803dee2d-9bd6-406a-b228-5572d5f1bbb2.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-d5917cb8-f00a-4691-a086-bf99f4962468.containerhub.tripleten-services.com"
 }
--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -1,11 +1,33 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
+const kitId = 2;
 
-const requestBody = {}
+const requestBody = {
+    "name": "My modified kit",
+    "productsList": [
+        {
+            "id": 1,
+            "quantity": 4
+        },
+        {
+            "id": 5,
+            "quantity": 2
+        },
+        {
+            "id": 3,
+            "quantity": 1
+        },
+        {
+            "id": 4,
+            "quantity": 1
+        }
+    ]
+};
 
-test('', async () => {
+test('status should be 200', async () => {
+	let response;
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		response = await fetch(`${config.API_URL}/api/v1/kits/${kitId}`, {
 			method: 'PUT',
 			headers: {
 			'Content-Type': 'application/json'
@@ -15,4 +37,7 @@ test('', async () => {
 	} catch (error) {
 		console.error(error);
 	}
+	const data = await response.json();
+	console.log(data);
+	expect(response.status).toBe(200);
 });

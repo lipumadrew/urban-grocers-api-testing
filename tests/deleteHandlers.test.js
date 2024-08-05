07ea49c97f2c7fba26a3c994a5778bc989bcb952
Andrew Lipuma <andrewlipuma@gmail.com>
2024-08-05 12:31:07 -0700

    Add tests for deleteHandlers

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,12 +1,27 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
+const kitID = 7;
 
-test('', async () => {
+test('response status should be 200', async () => {
+	let response;
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		response = await fetch(`${config.API_URL}/api/v1/kits/${kitID}`, {
 			method: 'DELETE',
 		});
 	} catch (error) {
 		console.error(error);
 	}
+	
+	expect(response.status).toBe(200);
 });
+
+test('deleted kit should no longer exist', async () => {
+	let response;
+	try {
+		response = await fetch(`${config.API_URL}/api/v1/kits/${kitID}`)
+	} catch (error) {
+		console.error(error);
+	}
+	expect(response.status).toBe(404);
+
+})
